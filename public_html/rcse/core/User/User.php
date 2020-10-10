0daac4c1ea5d8a02d46925cb17017b7d28672cbb
LoneSimba <vovamaster99@gmail.com>
2020-10-10 10:25:15 +0300

    Small advancements on User system (not really)

--- public_html/rcse/core/User/User.php
+++ public_html/rcse/core/User/User.php
@@ -7,19 +7,14 @@ use RCSE\Core\Database\Database;
 class User
 {
 
-    public $userData = [];
     private $db;
+    private $login;
     
     public function __construct(string $login, Database $db)
     {
         $this->db = $db;
-        $this->getUserData($login);
-    }
-
-    public function updateUserData()
-    {
-        $this->db->addQueryData('upd_user_by_login',$this->userData);
-        $this->db->executeAndGetResult('upd_user_by_login');
+        $this->login = $login;
+        
     }
 
     /**
@@ -30,14 +25,20 @@ class User
      */
     public function hasPermission(string $permission) : bool
     {
-        return (bool) array_search($permission, $this->userData['user_perms']);
+    
     }
 
-    private function getUserData(string $login) : void
+    public function isUserCreditansCorrect(string $login, string $pass)
+    {
+
+    }
+
+    public function getUserData(string $login) : void
     {
         $this->db->addQueryData('sel_user_by_login', ['login' => $login]);
         $this->userData = $this->db->executeAndGetResult('sel_user_by_login');
         $this->userData['user_prefs'] = json_decode($this->userData['user_prefs']);
         $this->userData['user_perms'] = json_decode($this->userData['user_perms']);
+        $this->userData['user_group'] = new UserGroup($this->userData['user_group'], $this->db);
     }
 }
