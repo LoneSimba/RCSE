58b439ccf7c30e09c4049e17eec74ff706a837b7
LoneSimba <vovamaster99@gmail.com>
2020-12-25 12:32:46 +0300

    Started work on new RCS default design: Created base structure prototype Updated various logo types Moved old design to old files

---
+++ public_html/resources/themes/rcs/prototypes/scripts/main.js
@@ -0,0 +1,35 @@
+$(function() {
+    setTimeout(function () {
+        toggleMenu("#head_menu_slider_button", "#head_menu_slider");
+    }, 250);
+
+   $("#head_menu_slider_button").on('click', function() {
+       toggleMenu("#head_menu_slider_button", "#head_menu_slider");
+   });
+
+   $("#head_account").on('click', function() {
+       toggleMenu("#head_account", "#head_account_popup", false);
+    });
+});
+
+function toggleMenu(acctButton, acctMenu, canBeClickedOutside = true) {
+    let accButton = $(acctButton)[0];
+    let accPopup = $(acctMenu)[0];
+
+    accButton.classList.toggle("active");
+    accPopup.classList.toggle("open");
+    if (!canBeClickedOutside) hideOnClickOutside(acctButton, acctMenu);
+}
+
+function hideOnClickOutside(acctButton, acctMenu) {
+    let accButton = $(acctButton)[0];
+    let accPopup = $(acctMenu)[0];
+
+    $(document).on('mouseup', function(e) {
+        if (!e.target.closest(acctMenu)?.length && $(acctMenu).is(':visible')) {
+            accButton.classList.remove('active');
+            accPopup.classList.remove('open');
+            $(document).off('mouseup');
+        }
+    });
+}
