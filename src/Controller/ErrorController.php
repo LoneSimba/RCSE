2cccd09c33f926a917b17d1521d94f107beafcd8
LoneSimba <vovamaster99@gmail.com>
2022-10-22 10:02:17 +0300

    error handler

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -9,6 +9,21 @@ class ErrorController extends AbstractController
 {
     public function show(\Throwable $exception, ?DebugLoggerInterface $debugLogger): JsonResponse
     {
+        $this->logger->error("Error occured: ({$exception->getCode()}) {$exception->getMessage()} in {$exception->getFile()} at {$exception->getLine()}, trace \n\n{$exception->getTraceAsString()}");
 
+        $data = $this->getParameter('kernel.environment') !== 'prod' ? [
+               'exception' => "{$exception->getCode()} - {$exception->getMessage()}" ,
+               'file' => $exception->getFile(),
+               'line' => $exception->getLine(),
+               'trace' => array_map(function ($item) {
+                   return [
+                       'line' => $item['line'] ?? null,
+                       'class' => $item['class'] ?? null,
+                       'function' => $item['function'] ?? null,
+                   ];
+               }, $exception->getTrace())
+           ] : [];
+
+        return $this->apiAnswer($data, false, 500, 'Internal error');
     }
 }
